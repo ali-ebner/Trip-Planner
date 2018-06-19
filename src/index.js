63c0b2a08d4ac9d1f9f513fbbf747f33ee82bb4f
Ali Ebner <ebner.ali@gmail.com>
2018-06-19 16:24:32 -0400

    adding the map

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('hello world');
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWxpLWVibmVyIiwiYSI6ImNqaW00N2JndTAwNnozcGxmd25heTBnMGMifQ.kMU4IYF3OdLLeY4JnSrpeQ';
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
